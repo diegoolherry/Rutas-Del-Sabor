263ae5cc0d2c7f8dae3bd2ae764a8cad8e8d40ca
EnzoFajardo278 <EnzoFajardo15@gmail.com>
2026-02-26 19:17:12 -0300

    Carpeta Home
    carpeta home

---
+++ app/Home/page.jsx
@@ -0,0 +1,57 @@
+'use client';
+
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { getLocales } from "../api/api";
+
+import Filters from "../components/Filters";
+import Header from "../components/Header";
+import LocalList from "../components/LocalList";
+
+export default function Home() {
+  const [token, setToken] = useState(null);
+  const [locales, setLocales] = useState([]);
+  const [cargando, setCargando] = useState(true);
+  const [filters, setFilters] = useState({
+    q: "",
+    type: "",
+    city: "",
+    priceRange: "",
+    rating: "",
+    zone: "",
+  });
+  const router = useRouter();
+ 
+  useEffect(() => {
+    const savedToken = localStorage.getItem("authToken");
+    setToken(savedToken);
+  }, []);
+
+  useEffect(() => {
+    const fetchLocales = async () => {
+      try {
+        setCargando(true);
+        const data = await getLocales(filters);
+        setLocales(data.items);
+      } catch (error) {
+        console.error("Error al obtener locales:", error);
+      }finally {
+        setCargando(false);
+      }
+    };
+
+    fetchLocales();
+  }, [filters]);
+
+  return (
+    <div>
+      <Header token={token} setToken={setToken} router={router} />
+
+      <h1>Bienvenido a Rutas del Sabor</h1>
+
+      <Filters filters={filters} setFilters={setFilters} />
+
+      <LocalList locales={locales} cargando={cargando} />
+    </div>
+  );
+}
